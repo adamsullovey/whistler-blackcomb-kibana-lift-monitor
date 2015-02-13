49821d750554b7f80a67b2c525f18b16db8a6bb4
Adam Sullovey <adam.sullovey@gmail.com>
2015-02-13 10:59:39 -0500

    'openssl md5' is adding some junk to the start of the hash string. Can I get rid of it with awk?

--- .profile.d/kibana-config.sh
+++ .profile.d/kibana-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sed -i 's|http://" + window.location.hostname + ":9200|'$BONSAI_URL'|g' public/config.js
-LOGIN=`echo -n $BONSAI_URL | cut -d "/" -f 3 | cut -d "@" -f 1 | openssl md5`
+LOGIN=`echo -n $BONSAI_URL | cut -d "/" -f 3 | cut -d "@" -f 1 | openssl md5 | awk {print $2}`
 echo "auth hash: $LOGIN"
 sed -i "s|loginhashhere|$LOGIN|g" public/config.js
