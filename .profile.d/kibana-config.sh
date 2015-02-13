f0f8be3ea65db832c5c7bbdcf2f9af8e391ef975
Adam Sullovey <adam.sullovey@gmail.com>
2015-02-13 10:51:44 -0500

    still debugging kibana-config.sh

--- .profile.d/kibana-config.sh
+++ .profile.d/kibana-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sed -i 's|http://" + window.location.hostname + ":9200|'$BONSAI_URL'|g' public/config.js
 LOGIN=`echo -n $BONSAI_URL | cut -d "/" -f 3 | cut -d "@" -f 1 | openssl md5`
-sed -i 's|loginhashhere|'$LOGIN'|g' public/config.js
-echo $LOGIN
+echo "auth hash: $LOGIN"
+sed -i "s|loginhashhere|$LOGIN|g" public/config.js
