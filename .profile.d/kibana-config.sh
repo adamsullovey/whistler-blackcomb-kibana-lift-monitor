6daa4777f75aa059a70d9c2876d32ea300d33ede
Adam Sullovey <adam.sullovey@gmail.com>
2015-02-13 10:36:42 -0500

    try to configure Kibana to log in to ElasticSearch

--- .profile.d/kibana-config.sh
+++ .profile.d/kibana-config.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 sed -i 's|http://" + window.location.hostname + ":9200|'$BONSAI_URL'|g' public/config.js
+LOGIN=`echo -n $BONSAI_URL | cut -d "/" -f 3 | cut -d "@" -f 1 | openssl md5`
+sed -i 's|loginhashhere|'$LOGIN'|g' public/config.js
