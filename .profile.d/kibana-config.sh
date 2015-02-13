652e8ba9ae7aefae1287f20c3feca1f7f2988590
Adam Sullovey <adam.sullovey@gmail.com>
2015-02-13 12:53:44 -0500

    finally got base64 encoding right (beware of linebreaks)

--- .profile.d/kibana-config.sh
+++ .profile.d/kibana-config.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 sed -i 's|http://" + window.location.hostname + ":9200|'$BONSAI_URL'|g' public/config.js
-LOGIN=`echo -n $BONSAI_URL | cut -d "/" -f 3 | cut -d "@" -f 1 | base64`
-echo "auth hash: $LOGIN"
-sed -i "s|loginhashhere|$LOGIN|g" public/config.js
+
+LOGIN=`echo -n "$BONSAI_URL" | cut -d "/" -f 3 | cut -d "@" -f 1`
+LOGIN_ENCODED=`echo -n $LOGIN | openssl enc -base64`
+
+
+echo "auth: $LOGIN"
+echo "auth encoded: $LOGIN_ENCODED"
+
+
+sed -i "s|loginhashhere|$LOGIN_ENCODED|g" public/config.js
